7a5f057b16a9ea4e21282cb4d96a0d7f2d5fa555
Sathwik Shetty <sathwikshetty9876@gmail.com>
2025-01-07 22:39:18 +0530

    added maker

---
+++ .github/workflows/token/jwt_maker.go
@@ -0,0 +1,37 @@
+package token
+
+import (
+	"fmt"
+	"time"
+	"github.com/golang-jwt/jwt"
+)
+
+const minSecretKeySize = 32
+
+type JWTMaker struct {
+	secretKey string
+}
+
+func NewJWTMaker(secretKey string) (Maker, error) {
+	if len(secretKey) < minSecretKeySize {
+		return nil, fmt.Errorf("invalid key size")
+	}
+	return &JWTMaker{secretKey}, nil
+}
+
+func (maker *JWTMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	return jwtToken.SignedString([]byte(maker.secretKey))
+}
+
+
+
+func(maker *JWTMaker) VerifyToken(token string) (string, error){
+	return "", nil
+}
+
+
--- .github/workflows/token/payload.go
+++ .github/workflows/token/payload.go
@@ -1,20 +1,29 @@
 package token
 
 import (
-	"time"
-
+	"fmt"
 	"github.com/google/uuid"
+	"time"
 )
 
 type Payload struct {
-	ID uuid.UUID `json:"id"`
+	ID        uuid.UUID `json:"id"`
 	Username  string    `json:"username"`
 	IssuedAt  int64     `json:"issuedAt"`
 	ExpiresAt time.Time `json:"expiresAt"`
 }
 
+
+
+func (p Payload) Valid() error {
+	if time.Now().After(p.ExpiresAt) {
+		return fmt.Errorf("token has expired")
+	}
+	return nil
+}
+
 func NewPayload(username string, duration time.Duration) (Payload, error) {
-	tk,err := uuid.NewRandom()
+	tk, err := uuid.NewRandom()
 	return Payload{
 		ID:        tk,
 		Username:  username,
